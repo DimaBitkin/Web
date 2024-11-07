d691dd0ecfd5cd8284a76bf8228ed1b4656967fb
DimaBitkin <dima-b.b2017@yandex.ru>
2024-11-07 15:45:59 +0300

    lab1

---
+++ lab1/script.js
@@ -0,0 +1,15 @@
+function calculateAbs() {
+    // Получаем значение введенное пользователем
+    let x = document.getElementById("inputNumber").value;
+    // Преобразуем его в число
+    x = parseFloat(x);
+    // Проверяем корректность введенного значения
+    if (isNaN(x)) {
+        document.getElementById("result").innerHTML = "Пожалуйста, введите корректное число.";
+    } else {
+        // Вычисляем абсолютное значение
+        let result = Math.abs(x);
+        // Выводим результат на страницу
+        document.getElementById("result").innerHTML = "Абсолютное значение: " + result;
+    }
+}
